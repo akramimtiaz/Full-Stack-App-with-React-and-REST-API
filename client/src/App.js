23531f119a16742394b22df9b5a97f081fc728d9
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-07-04 07:47:21 +1000

    Install Joi validator

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 //router
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //components
 import Header from './components/Header'
 import Courses from './components/Courses'
 import CourseDetail from './components/CourseDetail'
+import CreateCourse from './components/CreateCourse'
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <Route component={Header}/>
-        <Route exact path="/" component={Courses}/>
-        <Route exact path="/courses/:id" component={CourseDetail}/>
+        <Switch>
+          <Route exact path="/" component={Courses}/>
+          <Route exact path="/courses/create" component={CreateCourse}/>
+          <Route path="/courses/:id" component={CourseDetail}/>
+        </Switch>
       </div>
     </BrowserRouter>
   )
--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -6,15 +6,66 @@
  * STATEFUL COMPONENT
  */
 import React, { Component } from 'react'
+//context
+import { AuthContext } from './context'
 
 class CreateCourse extends Component {
-    render(){
-        return (
-            <div>
+    constructor() {
+        super()
+        this.state = {
+            title: '',
+            description: '',
+            estimatedTime: '',
+            materialsNeeded: '',
+        }
+    }
 
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        
+    }
+
+    handleCancel = (e) => {
+        this.props.history.push("/")
+    }
+
+    render(){
+        const { isAuth, authUser } = this.context
+        return (
+            <div className="createCourse--wrapper">
+                <div className="createCourse">
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="createCourse--left">
+                            <p>Course</p>
+                            <input type="text" name="title" placeholder="Course title..." value={this.state.title} onChange={this.handleChange}/>
+                            <p>By {isAuth ? authUser.firstName : 'N/A'}</p>
+                            <textarea type="text" name="description" placeholder="Course description..." 
+                            value={this.state.description} onChange={this.handleChange}></textarea>
+                        </div>
+                        <div className="createCourse--right">
+                            <div>Estimated Time<br/>
+                                <textarea type="text" name="estimatedTime" id="estimatedTime" placeholder="Hours..." 
+                                 value={this.state.estimatedTime} onChange={this.handleChange}></textarea>
+                            </div>
+                            <div>Materials Needed<br/>
+                                <textarea type="text" name="materialsNeeded" id="materialsNeeded" placeholder="List Materials..." 
+                                 value={this.state.materialsNeeded} onChange={this.handleChange}></textarea>
+                            </div>
+                        </div>
+                            <button name="update" type="submit" className="createCourse--button">Update</button>
+                    </form>
+                    <button name="cancel" onClick={this.handleCancel} className="createCourse--button">Cancel</button>
+                </div>
             </div>
         )
     }
 }
+CreateCourse.contextType = AuthContext
 
 export default CreateCourse
