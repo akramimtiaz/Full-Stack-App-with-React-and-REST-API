6966d29956d9c38ba911f34945539f0a61031c09
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-07-10 18:14:42 +1000

    Add Comments to API Requests Describing Each Function

--- client/src/components/api/index.js
+++ client/src/components/api/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-//GET All Courses 
+
+/** 
+     * Retrieves all Courses 
+     * @return  {Promise}   a Promise object that is either resolved or rejected dependent on the result of the request.
+     *                      In the event the request is successful, the courses are returned, else an error is returned.
+*/
 export const getCourses = () => {
     return new Promise((resolve, reject) => {
         axios({
@@ -21,7 +26,12 @@ export const getCourses = () => {
     })
 }
 
-//GET Course that Matches ID
+/** 
+     * Retrieves the course that matches the provided ID
+     * @param    integer    indicating the ID of the course to be retrieved
+     * @return  {Promise}   a Promise object that is either resolved or rejected dependent on the result of the request.
+     *                      In the event the request is successful, the course is returned, else the response status is returned.
+*/
 export const getCourse = (id) => {
     return new Promise((resolve, reject) => {
         axios({
@@ -44,7 +54,13 @@ export const getCourse = (id) => {
     })
 }
 
-//POST Create a New Course
+/** 
+     * Creates a new course
+     * @param   object      course object containing information about the course to be created
+     * @param   object      user object containing information about the user creating the course
+     * @return  {Promise}   a Promise object that is either resolved or rejected dependent on the result of the request.
+     *                      However, irrespective of the result, the response is returned.
+*/
 export const createCourse = (course, user) => {
     return new Promise((resolve, reject) => {
         axios({
@@ -72,7 +88,15 @@ export const createCourse = (course, user) => {
     })
 }
 
-//PUT Update an Existing Course
+
+/** 
+     * Updates a course that matches the provided ID
+     * @param   integer     indicating the ID of the course to be updated
+     * @param   object      course object containing the updated course information
+     * @param   object      user object containing information about the user requesting the update
+     * @return  {Promise}   a Promise object that is either resolved or rejected dependent on the result of the request.
+     *                      However, irrespective of the result, the response is returned.
+*/
 export const updateCourse = (id, course, user) => {
     return new Promise((resolve, reject) => {
         axios({
@@ -100,7 +124,12 @@ export const updateCourse = (id, course, user) => {
     })
 }
 
-//DELETE Course with Matching ID
+/** 
+     * Deletes a course that matches the provided ID
+     * @param   integer     indicating the ID of the course to be deleted
+     * @param   object      user object containing information about the user deleting the course
+     * @return  {Promise}   a Promise object that is either resolved or rejected dependent on the result of the request.
+*/
 export const deleteCourse = (id, user) => {
     return new Promise((resolve, reject) => {
         axios({
@@ -127,7 +156,13 @@ export const deleteCourse = (id, user) => {
     })
 }
 
-//GET User with Matching Credentials
+/** 
+     * Retrieves the user that matches the provided email
+     * @param    string     email of the user to be authenticated
+     * @param    string     email of the associated user account
+     * @return  {Promise}   a Promise object that is either resolved or rejected dependent on the result of the request.
+     *                      In the event the request is successful, the user data is returned, else the response status is returned.
+*/
 export const getUser = (email, password) => {
     return new Promise((resolve, reject) => {
         axios({
@@ -154,7 +189,12 @@ export const getUser = (email, password) => {
     })
 }
 
-//POST Create a New User
+/** 
+     * Creates a new user
+     * @param   object      user object containing information about the user to be created
+     * @return  {Promise}   a Promise object that is either resolved or rejected dependent on the result of the request.
+     *                      However, irrespective of the result, the response is always returned.
+*/
 export const createUser = (user) => {
     return new Promise((resolve, reject) => {
         axios({
